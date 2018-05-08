634522047d6e0f46367b766d65849ec3a3e45c87
darekSkiciak <dariusz.skiciak@gmail.com>
2018-05-08 15:45:23 +0200

    - install redux - redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+
 import './index.css';
 import App from './container/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
