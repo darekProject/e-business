445675505548166a7b7f3d110dfb7efcc94f0543
darekSkiciak <dariusz.skiciak@gmail.com>
2018-05-08 16:52:01 +0200

    - add variable sass - install router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
+import {Route, BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './container/App';
@@ -14,7 +15,11 @@ const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <Fragment>
+                <Route path="/" component={App}/>
+            </Fragment>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
